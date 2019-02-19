26082312ae4181c7b191f95df04f11b97c75d9e4
AkshayaRajagopalan <a2rajago@edu.uwaterloo.ca>
2019-02-18 21:33:36 -0500

    movement with inclusion of gravity

--- first.py
+++ first.py
@@ -1,11 +1,19 @@
 from visual import *
-ball = sphere(pos=(0,10,0), radius=1, color=color.red)
-dt = 0.1
-ball.velocity = vector(0,0,0)
-t=0
 
-while (t < 10000):
-    rate(10)
-    ball.pos.y = 15*sin(t)
-    ball.pos.x = 30*cos(t)
-    t = t+dt
+scene = display(width = 600, height = 600, center = (0,5,0))
+
+sun = sphere(pos=(0,0,0), radius=100, color = color.orange)
+earth = sphere(pos=(-200,0,0), radius=10, materials = materials.earth,
+               make_trail = true)
+
+earthv = vector(0,0,5)
+
+for i in range(10000):
+    rate(100)
+    earth.pos = earth.pos + earthv
+    dist = (earth.x**2 + earth.y**2 + earth.z**2)**0.5
+    RadialVector = (earth.pos - sun.pos)/dist
+    Fgrav = -10000*RadialVector/dist**2
+    earthv = earthv + Fgrav
+    earth.pos += earthv
+    if dist <= sun.radius: break
